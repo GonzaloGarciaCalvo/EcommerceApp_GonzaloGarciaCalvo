07d1113b5da5cfe4dc637726994be56186ba4d7c
Gonzalo <garciacalvog@hotmail.com>
2022-05-26 13:28:42 -0300

    Go back en flujo scroll de la FlatList, ListFooterComponent...

--- Components/List/index.js
+++ Components/List/index.js
@@ -2,6 +2,7 @@ import { FlatList, StyleSheet, Text, Touchable, TouchableOpacity, View } from 'r
 import React from 'react'
 import CategoryItem from './CategoryItem'
 import ProductItem from './ProductItem'
+import MyButton from '../MyButton'
 
 const List = ({ itemType = "category", data, onPress}) => {
 
@@ -22,6 +23,7 @@ const List = ({ itemType = "category", data, onPress}) => {
             data={data}
             renderItem={fnRender}
             keyExtractor={item => item.id}
+            ListFooterComponent={itemType === "category" ? null:<MyButton/> }
         />
     )
 }
